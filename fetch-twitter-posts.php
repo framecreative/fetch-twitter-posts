a5bcdf67ad754c40709a191c78dc9281394b6189
Luke Smith <contact@luke-smith.com>
2022-03-22 16:02:20 +1030

    Version bump v1.0.6

--- fetch-twitter-posts.php
+++ fetch-twitter-posts.php
@@ -4,7 +4,7 @@
 
 Plugin Name: Fetch Twitter Posts
 Plugin URI: http://framecreative.com.au
-Version: 1.0.5
+Version: 1.0.6
 Author: Frame
 Author URI: http://framecreative.com.au
 Description: Fetch latest posts from Twitter and save them in WP
